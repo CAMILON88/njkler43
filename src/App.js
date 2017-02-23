34f341ce6615080c29336c3ca62a859ba3665c7e
Juan Gomez Aranzazu <j.gomez267@gmail.com>
2017-02-23 09:28:16 -0500

    VERSION DE PRUEBA FINALIZADA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
-import posts from './posts'
+import posts from './posts';
 
 
-// Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+     
+     busqueda: '',
+     post: posts,
+     
+
+    }
+
+
+
+  }
+
+
+
+
+   textChange(event) {
+  
+  
+    this.setState({
+      busqueda: event.target.value,
+     
+      
+  })
+
+ 
+
+  }
+
+
+
   render() {
+  
+
+
+  
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.busqueda} onChange={this.textChange.bind(this)} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          
+           {this.state.post.filter((contacto) => {return contacto.title.toLowerCase().indexOf(this.state.busqueda.toLowerCase()) != -1; }).map((list, index) => <li  key={index}>  <a href={list.url}><img src={list.image } /></a>  <p>{ list.title }</p> </li>)}
+            
+           
+          
         </ul>
       </div>
     )
   }
 }
+
 
 
 export default App
